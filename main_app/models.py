f51ec269366f4e495d8db1a41a036dea3eb40ecb
MN <magmarnowak@gmail.com>
2017-05-03 11:18:17 +0200

    Set up template inheritance and submission form

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,4 @@ class Treasure(models.Model):
     name = models.CharField(max_length=100)
     material = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    img_url = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
+    img_url = models.CharField(max_length=255)
