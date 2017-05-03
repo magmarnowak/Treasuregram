bd815b8d66da04ffbf2a4ca4e747f0f6e78618ef
MN <magmarnowak@gmail.com>
2017-05-03 20:34:40 +0200

    Add image upload

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Treasure(models.Model):
     value = models.DecimalField(max_digits=10, decimal_places=2)
     name = models.CharField(max_length=100)
     material = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    img_url = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='treasure_images', default='media/default.png')
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
 # import the views module from the current app (main_app):
 from . import views
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^([0-9]+)/$', views.detail, name = 'detail'),
     url(r'^post_url/$', views.post_treasure, name='post_treasure'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT,}),
+    ]
