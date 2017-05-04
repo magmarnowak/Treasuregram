305e9180fdac509a308a37b7e58f690e2d513c93
MN <magmarnowak@gmail.com>
2017-05-04 14:17:18 +0200

    Add logout

--- main_app/views.py
+++ main_app/views.py
@@ -58,3 +58,7 @@ def register(request):
     else:
         form = UserCreationForm()
         return render(request, 'registration.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
